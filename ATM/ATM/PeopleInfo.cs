b817f1d24546f9007208b9af02c3a59a78b37513
yaminet1024 <yaminet@sina.com>
2016-12-30 22:35:40 +0800

    Creat by c#

---
+++ ATM/ATM/PeopleInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    class PeopleInfo
+    {
+        private string account;
+        private string name;
+        private string ID;
+        private string passsword;
+
+        public string Account
+        {
+            get
+            {
+                return account;
+            }
+
+            set
+            {
+                account = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+}
